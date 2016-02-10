e99035c9bbfb391af2467240dd3cd856505bcfb0
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-02-10 07:02:13 +0100

    wagon-git commit

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.ignite", "org/mybatis/caches/ignite/package-summary.html", [ [ "Classes", null, [ [ "IgniteCacheAdapter", "org/mybatis/caches/ignite/IgniteCacheAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,5 @@
+var DATA = [
+      { id:0, label:"org.mybatis.caches.ignite", link:"org/mybatis/caches/ignite/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.caches.ignite.IgniteCacheAdapter", link:"org/mybatis/caches/ignite/IgniteCacheAdapter.html", type:"class" }
+
+    ];
