094834a3a1ecca3d63adc5ece1a8107d49ebcedf
shtykh_roman <rshtykh@yahoo.com>
2016-03-08 18:51:35 +0900

    Ignite configuration file path to a variable.

--- src/main/java/org/mybatis/caches/ignite/IgniteCacheAdapter.java
+++ src/main/java/org/mybatis/caches/ignite/IgniteCacheAdapter.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2016 the original author or authors.
+ * Copyright 2016 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.caches.ignite;
 
@@ -53,6 +53,9 @@ public final class IgniteCacheAdapter implements Cache {
     /** Cache. */
     private final IgniteCache cache;
 
+    /** Ignite configuration file path. */
+    private static final String cfgPath = "config/default-config.xml";
+
     /**
      * Constructor.
      *
@@ -68,8 +71,7 @@ public final class IgniteCacheAdapter implements Cache {
         try {
             DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 
-            new XmlBeanDefinitionReader(factory).loadBeanDefinitions(new FileSystemResource(
-                new File("config/default-config.xml")));
+            new XmlBeanDefinitionReader(factory).loadBeanDefinitions(new FileSystemResource(new File(cfgPath)));
 
             cacheCfg = (CacheConfiguration)factory.getBean("templateCacheCfg");
 
@@ -82,8 +84,7 @@ public final class IgniteCacheAdapter implements Cache {
         }
         catch (NoSuchBeanDefinitionException | BeanDefinitionStoreException e) {
             // initializes the default cache.
-            log.warn("Initializing the default cache. " +
-                "Consider properly configuring 'config/default-config.xml' instead.");
+            log.warn("Initializing the default cache. Consider properly configuring '" + cfgPath + "' instead.");
 
             cacheCfg = new CacheConfiguration(id);
         }
