135780d4effd36bbb22c5531ead03bf0afce6d2e
hazendaz <jeremylandis@hotmail.com>
2024-02-10 22:52:16 +0000

    Deploying to gh-pages from @ mybatis/ignite-cache@e8f0d5f3abc214839e49eb1cbf0993adeb52cb00 🚀

--- apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.mybatis.caches.ignite", "org/mybatis/caches/ignite/package-summary.html", [ [ "Classes", null, [ [ "IgniteCacheAdapter", "org/mybatis/caches/ignite/IgniteCacheAdapter.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- apidocs/reference/lists.js
+++
@@ -1,5 +0,0 @@
-var DATA = [
-      { id:0, label:"org.mybatis.caches.ignite", link:"org/mybatis/caches/ignite/package-summary.html", type:"package" },
-      { id:1, label:"org.mybatis.caches.ignite.IgniteCacheAdapter", link:"org/mybatis/caches/ignite/IgniteCacheAdapter.html", type:"class" }
-
-    ];
